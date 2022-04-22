d93f3408f9eece190b34123451923f49830955d6
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-04-22 22:15:20 +0600

    node server is ready

--- __steps.js
+++ __steps.js
@@ -13,7 +13,7 @@
  * 1. require express
  * 2. create app variable
  * 3. declare port
- * 4. set app.get('/)
+ * 4. set app.get('/')
  * 5. listen to port
  * 6. node index.js
  * 7. check your browser for that port
